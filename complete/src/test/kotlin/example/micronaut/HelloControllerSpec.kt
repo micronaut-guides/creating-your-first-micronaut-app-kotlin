d1ce145f6c7567a30347744c2c24bd935bdedc7e
sdelamo <sergio.delamo@softamo.com>
2018-08-21 18:01:54 +0200

    Upgrade to Spek 2

--- complete/src/test/kotlin/example/micronaut/HelloControllerSpec.kt
+++ complete/src/test/kotlin/example/micronaut/HelloControllerSpec.kt
@@ -3,23 +3,23 @@ package example.micronaut
 import io.micronaut.context.ApplicationContext
 import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.on
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
 
-class HomeControllerSpec: Spek({
-    describe("HelloController") {
-        var embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java) // <1>
-        var client : HttpClient  = HttpClient.create(embeddedServer.url) // <2>
-        on("test /hello responds Hello World") {
+object BooksControllerSpec: Spek({
+    describe("BookController Suite") {
+        var embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
+        var client : HttpClient = HttpClient.create(embeddedServer.url)
+
+        it("test /hello responds Hello World") {
             var rsp : String = client.toBlocking().retrieve("/hello")
             assertEquals(rsp, "Hello World")
         }
+
         afterGroup {
             client.close()
             embeddedServer.close()
         }
-
     }
 })
