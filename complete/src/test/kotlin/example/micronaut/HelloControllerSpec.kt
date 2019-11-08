8da9a9b069b27af431061e35625195b2bf8803a9
Iván López <lopezi@objectcomputing.com>
2019-11-08 13:24:26 +0100

    Fix spek2 imports

--- complete/src/test/kotlin/example/micronaut/HelloControllerSpec.kt
+++ complete/src/test/kotlin/example/micronaut/HelloControllerSpec.kt
@@ -3,9 +3,10 @@ package example.micronaut
 import io.micronaut.context.ApplicationContext
 import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
-import kotlin.test.assertEquals
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.jupiter.api.Assertions.assertEquals
 
 object HelloControllerSpec: Spek({
     describe("HelloController Suite") {
