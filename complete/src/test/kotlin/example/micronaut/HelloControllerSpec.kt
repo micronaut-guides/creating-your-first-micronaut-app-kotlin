ac056b39139b8ee4750a42e33be14e9388f073be
Iván López <lopezi@objectcomputing.com>
2019-11-19 13:57:33 +0100

    Use Spek2

--- complete/src/test/kotlin/example/micronaut/HelloControllerSpec.kt
+++ complete/src/test/kotlin/example/micronaut/HelloControllerSpec.kt
@@ -3,18 +3,17 @@ package example.micronaut
 import io.micronaut.context.ApplicationContext
 import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
 
-object HelloControllerSpec: Spek({
+object HelloControllerSpec : Spek({
     describe("HelloController Suite") {
-        var embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java) // <1>
-        var client : HttpClient = HttpClient.create(embeddedServer.url) // <2>
+        var embeddedServer: EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java) // <1>
+        var client: HttpClient = HttpClient.create(embeddedServer.url) // <2>
 
         it("test /hello responds Hello World") {
-            var rsp : String = client.toBlocking().retrieve("/hello")
+            var rsp: String = client.toBlocking().retrieve("/hello")
             assertEquals("Hello World", rsp)
         }
 
